ef212ac2df4f4c88f64376d977825b2782942e3a
sans1960 <asanscliment@gmail.com>
2022-04-12 15:30:52 +0200

    view post

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.front')
+@extends('layouts.post')
 @section('content')
 
 
-<div class="container flex flex-col w-3/5 mx-auto mt-5 mb-5 border-b-4 border-b-gray-700">
+<div class="container flex flex-col w-3/4 mx-auto mt-5 mb-5 border-b-4 border-b-gray-700">
     <h1 class="mt-5 mb-5 text-3xl text-center font-patua-one">{{ $post->title }}</h1>
     <div class="flex flex-col items-center justify-center mt-5 mb-5 text-gray-600 md:flex-row font-patua-one">
 
@@ -21,8 +21,8 @@
      <div class="p-3 tracking-wide text-gray-600 font-open-sans indent-0">
         {!! $post->body !!}
     </div>
-    <div class="mx-auto mt-4 mb-5">
-        <a class="px-8 py-2 tracking-wider bg-white border-2 border-gray-900 cursor-pointer rounded-3xl hover:bg-gray-800 hover:text-white font-patua-one" href="{{ route('contact',[$post->slug,$post->subregion_id,$post->country_id]) }}">Start to plan my trip</a>
+    <div class="mx-auto mt-4 mb-5 flex justify-center items-center">
+        <a class="text-sm md:text-base px-8 py-2 tracking-wider bg-white border-2 border-gray-900 cursor-pointer rounded-3xl hover:bg-gray-800 hover:text-white font-patua-one" href="{{ route('contact',[$post->slug,$post->subregion_id,$post->country_id]) }}">Start to plan my trip</a>
     </div>
     <div id="map" class="flex justify-center mx-auto" style="width: 100%;height:400px;">
 
