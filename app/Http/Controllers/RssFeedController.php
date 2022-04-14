d3f02c564a7e1a98f2400c41460fe64b48968289
sans1960 <asanscliment@gmail.com>
2022-04-14 17:14:00 +0200

    feed rss europe

--- app/Http/Controllers/RssFeedController.php
+++ app/Http/Controllers/RssFeedController.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 class RssFeedController extends Controller
 {
     public function rssEurope(){
-        $posts = Post::where('destination_id',1)->get();
+        $posts = Post::where('destination_id',1)->orderBy('date','ASC')->get();
         return response()->view('feed.europe',compact('posts'))->header('Content-Type','application/xml');
     }
 }
--- resources/views/feed/europe.blade.php
+++ resources/views/feed/europe.blade.php
@@ -1,22 +1,22 @@
-
-<?=
-'<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
-?>
+{!! '<'.'?xml version="1.0"?>' !!}
 <rss version="2.0">
     <channel>
         <title><![CDATA[ SoujournPlanet ]]></title>
-        <link><![CDATA[ https://localhost/feed/europe ]]></link>
+        <link><![CDATA[{{  env('APP_URL') }}/feed/europe ]]></link>
         <description><![CDATA[ Blog ]]></description>
         <language>en</language>
         <pubDate>{{ now()->toDayDateTimeString('Europe/Madrid') }}</pubDate>
 
         @foreach($posts as $post)
             <item>
-                <title><![CDATA[{{ $post->title }}]]></title>
-                <link>https://localhost/post/{{ $post->slug }}</link>
+                <title><![CDATA[{!! $post->title !!}]]></title>
+                  <description> <![CDATA[<img src="{{ asset('public/storage/photos/'.$post->photo->image) }}" width="100" height="50"/>]]> </description>
+                <link>{{ env('APP_URL') }}/post/{{ $post->slug }}</link>
 
-                <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
+
+                <date>{{ $post->created_at->toRssString() }}</date>
             </item>
         @endforeach
     </channel>
 </rss>
+
