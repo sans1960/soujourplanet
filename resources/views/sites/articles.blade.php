57edc46a2caddbc92090b94278b04ececbae22a5
sans1960 <asanscliment@gmail.com>
2022-05-31 15:16:51 +0200

    styles Articles

--- resources/views/sites/articles.blade.php
+++ resources/views/sites/articles.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.essential')
 @section('content')
-<div class="container  w-3/4 mx-auto mt-5 flex flex-col">
+<div class="container flex flex-col w-3/4 mx-auto mt-5">
 
     @foreach ($pag as $item)
-        <div>
-            <h1 class="text-2xl text-center font-patua-one mb-6">{{ $item->name }}</h1>
-            {{-- <figure>
-                <img src="{{ asset('storage/pages/'.$item->image) }}" alt="Trulli" class="w-1/2 mx-auto mb-4">
-                <figcaption class="text-center mb-4">{{ $item->caption }}</figcaption>
-              </figure> --}}
+        <div class="w-3/4 mx-auto">
+            <h1 class="mb-6 text-3xl text-center font-patua-one">{{ $item->name }}</h1>
+            <div class="w-3/4 p-3 mx-auto text-xl tracking-wide font-open-sans indent-0">
+                {!! $item->description !!}
+            </div>
         </div>
-        <div class="p-3 text-lg font-open-sans border-b-4 border-b-gray-400">
-            {!! $item->body !!}
-        </div>
+
 
 
         @foreach ($item->article as $it)
-        <div>
-            <h1 class="text-2xl text-center font-patua-one mb-6">{{ $it->name }}</h1>
+        <div class="flex flex-col w-3/4 mx-auto mt-5">
+            <h1 class="mb-6 text-2xl text-center font-patua-one"> {{ $it->order }}  - {{ $it->name }}</h1>
             <figure>
-                <img src="{{ asset('storage/articles/'.$it->image) }}" alt="Trulli" class="w-1/2 mx-auto mb-4">
-                <figcaption class="text-center mb-4">{{ $it->caption }}</figcaption>
+                <img src="{{ asset('storage/articles/'.$it->image) }}" alt="Trulli" class="w-1/2 mx-auto mb-2">
+                <figcaption class="mb-4 text-center">{{ $it->caption }}</figcaption>
               </figure>
+              <div class="w-3/4 p-4 mx-auto text-xl tracking-wide indent-0 font-open-sans ">
+                {!! $it->body !!}
+            </div>
         </div>
-        <div class="p-3 text-lg font-open-sans border-b-4 border-b-gray-400 w-3/4 mx-auto">
-            {!! $it->body !!}
-        </div>
+
 
         @endforeach
 
